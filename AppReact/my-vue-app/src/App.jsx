e4620e34bf3a5b32ab0c3fc5d8aac7c80a186cfe
GimmiPrud <prudenzig@gmail.com>
2024-11-21 13:39:02 +0100

    ....

--- AppReact/my-vue-app/src/App.jsx
+++ AppReact/my-vue-app/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavBar from './componente/NavBar.Jsx'
+import NavBar from './componente/NavBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <NavBar></NavBar>
+    <NavBar></NavBar> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
