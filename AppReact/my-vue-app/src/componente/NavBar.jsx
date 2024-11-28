10d5e3370ebcf5fc249ff911674a4a4becfb2e4d
GimmiPrud <prudenzig@gmail.com>
2024-11-28 18:12:57 +0100

    ,,,

--- AppReact/my-vue-app/src/componente/NavBar.jsx
+++ AppReact/my-vue-app/src/componente/NavBar.jsx
@@ -6,18 +6,15 @@ const img1 = '/vite.svg'
 const img2 = '/react.svg'
 
 const imgStyle = {
-    height: '100xp',
-    borderRadius: '50'
-    
+    height: '300px',
+    borderRadius: '90px'
 }
-
-
 
 function NavBar(){
     return (
         <>
         <nav>{x}</nav>  
-        <img src ={img1} height={200} ></img>
+        <img className='bordoArrotondato' src ={img1} height={200} ></img>
         <img style={imgStyle}  src ={img2}></img>
         <ul>
             <li><a href= "#"> ciao </a></li>
