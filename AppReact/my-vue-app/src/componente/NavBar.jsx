5d2012d4e5746e3ab5ca39912b22a8717a846ba3
GimmiPrud <prudenzig@gmail.com>
2024-11-28 16:51:00 +0100

    .....

--- AppReact/my-vue-app/src/componente/NavBar.jsx
+++ AppReact/my-vue-app/src/componente/NavBar.jsx
@@ -1,14 +1,20 @@
 import './NavBar.css'
+const x = 150;
+const img = '/assests/react.svg'
+// creazione constanti 
 
 function NavBar(){
     return (
-
+        <>
+        <nav>{x}</nav>  
+        <img src ={img}></img>
         <ul>
             <li><a href= "#"> ciao </a></li>
             <li><a href= "#"> ciao </a></li>
             <li><a href= "#"> ciao </a></li>
             <li><a href= "#"> ciao </a></li>
         </ul>
+        </>
     )
 }
 export default NavBar
