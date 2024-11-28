4f438d119606a0eaffb6469d46dfa902dfe9e6e1
GimmiPrud <prudenzig@gmail.com>
2024-11-28 18:09:37 +0100

    ,,,

--- AppReact/my-vue-app/src/componente/NavBar.jsx
+++ AppReact/my-vue-app/src/componente/NavBar.jsx
@@ -1,15 +1,24 @@
 import './NavBar.css'
+
+// creazione constanti 
 const x = '|-------------------------------------|';
 const img1 = '/vite.svg'
 const img2 = '/react.svg'
-// creazione constanti 
+
+const imgStyle = {
+    height: '100xp',
+    borderRadius: '50'
+    
+}
+
+
 
 function NavBar(){
     return (
         <>
         <nav>{x}</nav>  
         <img src ={img1} height={200} ></img>
-        <img src ={img2} height={120} ></img>
+        <img style={imgStyle}  src ={img2}></img>
         <ul>
             <li><a href= "#"> ciao </a></li>
             <li><a href= "#"> ciao </a></li>
