edd44ad438e59678cacdb51b933f537254a4444d
GimmiPrud <prudenzig@gmail.com>
2024-11-28 18:30:37 +0100

    ....

--- AppReact/my-vue-app/src/componente/NavBar.jsx
+++ AppReact/my-vue-app/src/componente/NavBar.jsx
@@ -14,8 +14,9 @@ function NavBar(){
     return (
         <>
         <nav>{x}</nav>  
-        <img className='bordoArrotondato' src ={img1} height={200} ></img>
+        <img src ={img1} height={200} ></img>
         <img style={imgStyle}  src ={img2}></img>
+        <p className='stilemio'> EVVAI </p>
         <ul>
             <li><a href= "#"> ciao </a></li>
             <li><a href= "#"> ciao </a></li>
