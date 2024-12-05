88e813cc316886e296dad5faf18f2d9a0b245436
GimmiPrud <prudenzig@gmail.com>
2024-12-05 12:01:53 +0100

    ...

--- Progect_React_01/my-vue-app/src/Components/Card.jsx
+++ Progect_React_01/my-vue-app/src/Components/Card.jsx
@@ -1,7 +1,7 @@
 
 function Card() {
-    const Titolo = "Ciao a tutti"
-
+    const Titolo = "CIAO A TUTTI"
+    
     return(
         <div>
             <p>
@@ -10,7 +10,6 @@ function Card() {
             </p>
             <img src="https://images.unsplash.com/photo-1730172233418-e23596ad0511?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"/>
         </div>
-
     )
 }
 export default Card;
