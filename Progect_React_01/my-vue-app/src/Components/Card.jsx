f809fbc53cbc26a086b038cf99f6d3233a62dde3
GimmiPrud <prudenzig@gmail.com>
2024-12-05 12:00:04 +0100

    ,

--- Progect_React_01/my-vue-app/src/Components/Card.jsx
+++ Progect_React_01/my-vue-app/src/Components/Card.jsx
@@ -1,9 +1,11 @@
 
 function Card() {
+    const Titolo = "Ciao a tutti"
+
     return(
-        
         <div>
             <p>
+                <h1>{Titolo}</h1>
                 <h1>BUON ANNO !!</h1>
             </p>
             <img src="https://images.unsplash.com/photo-1730172233418-e23596ad0511?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"/>
