cdc3e89489cb91d54a418a58825e88c38a8915d6
GimmiPrud <prudenzig@gmail.com>
2024-12-10 11:56:37 +0100

    ....

--- Progect_React_01/my-vue-app/src/App.jsx
+++ Progect_React_01/my-vue-app/src/App.jsx
@@ -79,6 +79,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => alert("BROCCOLO")}>
+          Clicca qui
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
