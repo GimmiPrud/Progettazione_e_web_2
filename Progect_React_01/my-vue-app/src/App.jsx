00acb7f3608309f4170a93a818a628a13b24a55a
GimmiPrud <prudenzig@gmail.com>
2024-12-10 12:42:27 +0100

    cf

--- Progect_React_01/my-vue-app/src/App.jsx
+++ Progect_React_01/my-vue-app/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       is_visit : false
     },
     {
-      id:1,
+      id:2,
       name:"London",
       desc:"Londra è una bella città",
       imgURL:"https://plus.unsplash.com/premium_photo-1671734045770-4b9e1a5e53a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9uZG9ufGVufDB8fDB8fHww",
