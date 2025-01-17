662061a77bada270c513c2264f6ff44624236232
GimmiPrud <prudenzig@gmail.com>
2025-01-17 16:58:28 +0100

    ,,,,

--- Rest_React_project/my-vue-app/src/App.jsx
+++ Rest_React_project/my-vue-app/src/App.jsx
@@ -8,9 +8,6 @@ function App() {
 
   return (
     <>
-    <body>
-      <Homepage></Homepage>
-    </body>
 
     </>
   )
--- Rest_React_project/my-vue-app/src/Home_Page/homepage.jsx
+++ Rest_React_project/my-vue-app/src/Home_Page/homepage.jsx
@@ -2,13 +2,10 @@
 import { useEffect , useState } from "react"
 
 function Homepage(){
-    const sfondo = "https://images.unsplash.com/photo-1531973576160-7125cd663d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG9mZmljZXxlbnwwfHwwfHx8MA%3D%3D"
 
     return(
         <>
-        <div>
-            <img src= {sfondo} alt=""/>
-        </div>
+
         </>
     )
 }
