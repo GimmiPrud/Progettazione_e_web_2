2bddf514ddd87f766fe5c7106d603ef1692db3eb
GimmiPrud <prudenzig@gmail.com>
2025-01-29 15:30:38 +0100

    ......

--- Rest_React_project/my-vue-app/src/App.jsx
+++ Rest_React_project/my-vue-app/src/App.jsx
@@ -3,11 +3,15 @@ import Axios from './Call_Rest/axios'
 import Persona from './Call_Rest/perosna'
 import Assenza from './Call_Rest/assenza'
 import Homepage from './Home_Page/homepage'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
+    <section>
+      <Navbar></Navbar>
+    </section>
     </>
   )
 }
