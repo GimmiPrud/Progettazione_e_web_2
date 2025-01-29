0cc5693a4e8f3c1849afee85bc87640b315f37db
GimmiPrud <prudenzig@gmail.com>
2025-01-29 16:31:25 +0100

    ...

--- Rest_React_project/React-App/src/App.jsx
+++ Rest_React_project/React-App/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './Components/Nav'
 
 function App() {
 
   return (
     <>
+    <section>
+      
+    </section>
     </>
   )
 }
