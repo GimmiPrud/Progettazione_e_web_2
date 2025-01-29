7ceb50b6cf9235446293449536bc8cf974155a03
GimmiPrud <prudenzig@gmail.com>
2025-01-29 15:34:47 +0100

    ...

--- Rest_React_project/my-vue-app/src/App.jsx
+++ Rest_React_project/my-vue-app/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
 
   return (
     <>
-    <section>
+    <head>
       <Navbar></Navbar>
-    </section>
+    </head>
     </>
   )
 }
--- Rest_React_project/my-vue-app/src/Components/Navbar.jsx
+++ Rest_React_project/my-vue-app/src/Components/Navbar.jsx
@@ -5,8 +5,9 @@ import React from 'react';
 const Navbar = () => {
     
     return (
-        <div>
-        </div>
+        <nav>
+            
+        </nav>
     );
 };
 
