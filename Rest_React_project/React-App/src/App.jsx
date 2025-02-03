b9d7ca4a92d2a09e98e027981606798f2ad03d75
GimmiPrud <prudenzig@gmail.com>
2025-02-03 13:57:49 +0100

    ,,,

--- Rest_React_project/React-App/src/App.jsx
+++ Rest_React_project/React-App/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navb from './Components/Nav'
 import Content_box from './Components/Content_box'
+import Card from './Components/Card'
 
 function App() {
 
@@ -10,6 +11,15 @@ function App() {
     <div>
       <Navb></Navb>
       <Content_box></Content_box>
+      <section style={{backgroundColor:"grey", padding:"6pc", display:"flex"}}>
+        <Card title={"ciao"} img ={"https://images.unsplash.com/photo-1659974708151-e90f42518dd0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fGRhdGFiYXNlfGVufDB8fDB8fHww"}></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </section>
+      <section style={{padding:"10pc" }}>
+
+      </section>
     </div>
     </>
   )
--- Rest_React_project/React-App/src/Components/Content_box.jsx
+++ Rest_React_project/React-App/src/Components/Content_box.jsx
@@ -4,10 +4,16 @@ import React from 'react';
 function Content_box() {
     
     return (
-        <div style={{backgroundColor:"whitesmoke", padding:"10px", borderRadius:"10px"}}>
-            <img className="img_c" src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZXxlbnwwfHwwfHx8MA%3D%3D"/>
-            
-        </div>
+        <div style={{display:"inline-flex", backgroundColor:"AppWorkspace", margin:"2pc", borderStyle:"solid",borderBottomRightRadius:"2pc", borderTopRightRadius:"2pc"}}>
+        <img className="img_c" src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZXxlbnwwfHwwfHx8MA%3D%3D"/>
+        <h3 style={{margin:"2pc", fontFamily:"inherit", fontSize:"2pc"}}>
+            Piattaforma Integrata per la Gestione Aziendale e dei Lavoratori.<p></p>
+            Officeer è una soluzione web innovativa progettata per semplificare la gestione dei dati aziendali e ottimizzare l'organizzazione dei lavoratori, rendendo i processi più efficienti, sicuri e scalabili.
+            <p></p>
+            Grazie alla nostra piattaforma, è possibile monitorare facilmente le informazioni relative ai dipendenti, come orari di lavoro, assenze, straordinari e performance
+        </h3>
+
+        </div> 
     );
 }
 
