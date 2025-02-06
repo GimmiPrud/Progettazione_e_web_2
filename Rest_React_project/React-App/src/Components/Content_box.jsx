df19ed25ee1c0234d42ef18f7efb0974c729e8a2
GimmiPrud <prudenzig@gmail.com>
2025-02-06 10:14:51 +0100

    ...

--- Rest_React_project/React-App/src/Components/Content_box.jsx
+++ Rest_React_project/React-App/src/Components/Content_box.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Content_box() {
     
     return (
-        <div style={{display:"inline-flex", backgroundColor:"AppWorkspace", margin:"2pc", borderStyle:"solid",borderBottomRightRadius:"2pc", borderTopRightRadius:"2pc"}}>
+        <div style={{display:"inline-flex", backgroundColor:"AppWorkspace", margin:"2pc", borderStyle:"solid"}}>
         <img className="img_c" src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZXxlbnwwfHwwfHx8MA%3D%3D"/>
         <h3 style={{margin:"2pc", fontFamily:"inherit", fontSize:"2pc"}}>
             Piattaforma Integrata per la Gestione Aziendale e dei Lavoratori.<p></p>
@@ -12,7 +12,6 @@ function Content_box() {
             <p></p>
             Grazie alla nostra piattaforma, è possibile monitorare facilmente le informazioni relative ai dipendenti, come orari di lavoro, assenze, straordinari e performance
         </h3>
-
         </div> 
     );
 }
