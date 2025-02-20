03f0214ef56687abb581a28cb73b42de8304aec6
GimmiPrud <prudenzig@gmail.com>
2025-02-20 16:09:51 +0100

    ....

---
+++ Rest_React_project/React-App/src/Components/Nav.jsx
@@ -0,0 +1,26 @@
+
+function Navb() {
+
+    return (
+        <div className="navbar">
+            <ul>
+                <li>
+                    Dipendenti
+                </li>
+                <li>
+                    Assenze
+                </li>
+                <li>
+                    Progetti
+                </li>
+                <li style={{paddingLeft:"8cap"}}>
+                    <span title='About'>
+                    <a style={{fontSize:"3.5pc"}} href="#">🖧</a>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navb;
